f54372ab14a3ad22e1c2e2832e224d4d1a77a5e0
Mataxca23 <martinataxcam1992@gmail.com>
2018-04-09 13:16:08 -0500

    No hacer caso
    sasas

--- tools/new-sku/register-new-sku.aspx.vb
+++ tools/new-sku/register-new-sku.aspx.vb
@@ -1,13 +1,37 @@
-﻿
+﻿Imports System.Collections.Generic
 Partial Class tools_Default
     Inherits System.Web.UI.Page
 
     Dim reader As New System.IO.StreamReader(HttpContext.Current.Request.InputStream)
     Dim requestFromPost As String = reader.ReadToEnd()
 
+    <System.Web.Services.WebMethod()>
+    Public Shared Function getData(ByVal datos As Datos) As Datos
+        Return datos
+    End Function
 
 End Class
 
+Public Class Datos
+    Private _name As String
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            _name = value
+        End Set
+    End Property
+    Private _population As Integer
+    Public Property Population As Integer
+        Get
+            Return _population
+        End Get
+        Set(ByVal value As Integer)
+            _population = value
+        End Set
+    End Property
+End Class
 Public Class Vendor
     Private _vendor As String
 
